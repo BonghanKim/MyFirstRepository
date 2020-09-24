ed43970b6b30c453113a35fecc0d255e837c3376
bonghan kim <kbonghan724@gmail.com>
2020-09-24 18:56:27 +0900

    Add Pointer_Test Project

---
+++ Test_3/Pointer_Test/Pointer_Test.cpp
@@ -0,0 +1,17 @@
+﻿// Pointer_Test.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int* myIntegerPointer = nullptr;
+	if (!myIntegerPointer)
+	{
+		myIntegerPointer = new int;
+	}
+	*myIntegerPointer = 9;
+
+	delete myIntegerPointer;
+	myIntegerPointer = nullptr;
+}
